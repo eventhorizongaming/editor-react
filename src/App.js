f8d3cd0ddd3e0a3e3cac026acf95ef530c310df6
Eli <136011323+EliasElliotson@users.noreply.github.com>
2024-10-09 23:44:02 +0000

    fix: added the sidebar elements to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
+import { Sidebar, SidebarTab } from './components/sidebar/'
+
+import { VscFiles } from "react-icons/vsc";
+import { VscSearch } from "react-icons/vsc";
+import { VscSourceControl } from "react-icons/vsc";
+import { VscDebugAlt } from "react-icons/vsc";
+import { VscExtensions } from "react-icons/vsc";
+import { VscGithubInverted } from "react-icons/vsc";
+
 function App() {
   return (
-    <h1>Hello, world!</h1>
+    <Sidebar>
+
+      <SidebarTab icon={<VscFiles />}>
+        <h1>Files</h1>
+      </SidebarTab>
+
+      <SidebarTab icon={<VscSearch />}>
+        <h1>Search</h1>
+      </SidebarTab>
+
+      <SidebarTab icon={<VscSourceControl />}>
+        <h1>Source control</h1>
+      </SidebarTab>
+
+      <SidebarTab icon={<VscDebugAlt />}>
+        <h1>Debugger</h1>
+      </SidebarTab>
+
+      <SidebarTab icon={<VscExtensions />}>
+        <h1>Extensions</h1>
+      </SidebarTab>
+
+      <SidebarTab icon={<VscGithubInverted />}>
+        <h1>GitHub</h1>
+      </SidebarTab>
+
+    </Sidebar>
   );
 }
 
